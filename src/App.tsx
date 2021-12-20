4be7a38ddc736bb6969a6937ef7ac81f5b050639
charles scheuer <charlesscheuer@charless-MacBook-Pro.local>
2021-12-19 21:12:12 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LottieControl from './Scroll.tsx'
+
+export default function App() {
+  return (
+    <div style={{height: 4000, backgroundColor: "#121315"}}>
+    <LottieControl />
+    </div>
+  )
+}
---
+++ src/Scroll.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+// import "./styles.css";
+import lottie from "lottie-web";
+import animationData from "./lottie-noGRID_BG.json";
+const LottieControl = () => {
+  const lottieRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    var animDuration = 2000;
+    const anim = lottie.loadAnimation({
+      container: lottieRef.current!,
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+
+      animationData
+    });
+
+    function animatebodymovin(duration: number) {
+      const scrollPosition = window.scrollY;
+      const maxFrames = anim.totalFrames;
+
+      const frame = (maxFrames / 100) * (scrollPosition / (duration / 100));
+      console.log(frame, "was frame")
+      if (frame < 0) {
+        anim.goToAndStop(1, true);
+      } else {
+        anim.goToAndStop(frame, true);
+      }
+
+      
+    }
+    const onScroll = () => {
+      console.log("Scrolling");
+      animatebodymovin(animDuration);
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => {
+      anim.destroy();
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return <div style={{ position: "fixed", width: "100%" }} ref={lottieRef}></div>;
+};
+
+export default LottieControl;
