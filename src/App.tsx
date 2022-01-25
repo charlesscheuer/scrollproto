cfddf35b19b6baac27aa0018bd5b808da4b23443
charles scheuer <charlesscheuer@charless-MacBook-Pro.local>
2022-01-24 16:41:22 -0800

    scrolling interactiong:

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import LottieControl from './Scroll.tsx'
 
 export default function App() {
   return [
-    <div style={{height: "4000px", width: "100vw", backgroundColor: "#121315"}}>
+    <div style={{height: "7000px", width: "100vw", backgroundColor: "#121315"}}>
     <LottieControl />
     </div>
 
