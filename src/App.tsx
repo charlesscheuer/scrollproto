f524de4d5b86a0c5d108aeecb1379f5a1934a636
charles scheuer <charlesscheuer@charless-MacBook-Pro.local>
2022-03-06 22:12:59 -0800

    adding the colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import LottieControl from './Scroll.tsx'
 
 export default function App() {
   return [
-    <div style={{height: "5000px", width: "100vw", backgroundColor: "#fff"}}>
+    <div style={{height: "5000px", width: "100vw",  top:0, left: 0, backgroundColor: "#111"}}>
+      <div style={{position: "fixed", top: 0, width: "100%", height: "auto"}}>
     <LottieControl />
+    </div>
     </div>
 
   ]
--- src/Scroll.tsx
+++ src/Scroll.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 // import "./styles.css";
 import lottie from "lottie-web";
-import animationData from "./SCROLL.json";
+import animationData from "./color.json";
 const LottieControl = () => {
   const lottieRef = React.useRef<HTMLDivElement>(null);
   // comitt
 
   React.useEffect(() => {
-    var animDuration = 2000;
+    var animDuration = 1000;
     const anim = lottie.loadAnimation({
       container: lottieRef.current!,
       renderer: "svg",
